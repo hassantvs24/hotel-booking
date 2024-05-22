8518cffc5ce8bc46c31bbd845ccddb79221b00d4
Nazmul Hossain <49582324+hassantvs24@users.noreply.github.com>
2024-05-23 02:45:14 +0600

    Update honeypot.php

--- config/honeypot.php
+++ config/honeypot.php
@@ -13,7 +13,7 @@ return [
      * value for this name will be discarded. Make sure this name does not
      * collide with a form field that is actually used.
      */
-    'name_field_name' => env('HONEYPOT_NAME', 'my_name'),
+    'name_field_name' => env('HONEYPOT_NAME', 'hotel_booking'),
 
     /*
      * When this is activated there will be a random string added
@@ -33,13 +33,13 @@ return [
      * if the form wasn't submitted too quickly. Make sure this name does not
      * collide with a form field that is actually used.
      */
-    'valid_from_field_name' => env('HONEYPOT_VALID_FROM', 'valid_from'),
+    'valid_from_field_name' => env('HONEYPOT_VALID_FROM', 'hotel_booking_form'),
 
     /*
      * If the form is submitted faster than this amount of seconds
      * the form submission will be considered invalid.
      */
-    'amount_of_seconds' => (int) env('HONEYPOT_SECONDS', 3),
+    'amount_of_seconds' => (int) env('HONEYPOT_SECONDS', 5),
 
     /*
      * This class is responsible for sending a response to requests that
