bd18f5b7705be3097b424eb01c53fe8e5cdbcc05
Nazmul Hossain <49582324+hassantvs24@users.noreply.github.com>
2024-05-23 04:45:26 +0600

    user profile

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,8 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'phone',
+        'profile_photo',
         'password',
         'user_type',
     ];
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,11 +14,14 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email')->nullable()->unique();
+            $table->string('phone')->nullable()->unique();
+            $table->string('profile_photo')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum('user_type', ['main_owner', 'hotel_owner', 'hotel_guest'])->default('hotel_guest');
             $table->rememberToken();
+            $table->softDeletes();
             $table->timestamps();
         });
 
