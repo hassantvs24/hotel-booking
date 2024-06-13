1dd47d7f8456f8a6ca2a0cb2aa00539fe2f290e0
Sukanta Purkayastha <spurkayastha83@gmail.com>
2024-06-13 10:22:18 +0600

    Update Helper.php

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -4,5 +4,9 @@ namespace App\Helpers;
 
 class Helper
 {
-
+    // Function to get the client IP address
+    public static function getUserIP() : ?string
+    {
+        return request()->ip();
+    }
 }
