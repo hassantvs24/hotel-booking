90bed218d17a55c46dfab98e7d49b52e5d122f2e
Milton Dey Mishu <mishuday307@gmail.com>
2024-06-22 22:37:06 +0600

    State CRUD done

--- app/Http/Controllers/Admin/Place/StateController.php
+++ app/Http/Controllers/Admin/Place/StateController.php
@@ -2,36 +2,82 @@
 
 namespace App\Http\Controllers\Admin\Place;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\BaseController;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use App\Http\Requests\Admin\Place\StateRequest;
+use App\Repositories\Place\CountryRepository;
+use App\Repositories\Place\StateRepository;
+use App\Models\State;
+use App\Models\Country;
 
-class StateController extends Controller
+
+class StateController extends BaseController
 {
     /**
      * Display a listing of the resource.
      */
-    public function index() :View
+    public function index(Request $request, StateRepository $stateRepository) :View
     {
+        if (!hasPermission('can_view_state')) {
+            $this->unauthorized();
+        }
 
-        return view('admin.place.state.index');
+        $query = array_merge(
+            $request->only(['search', 'filters', 'order_by', 'order', 'per_page', 'page']),
+            [
+                'with'     => [],
+                'where'    => [],
+                'order_by' => 'id',
+                'order'    => 'DESC',
+            ]
+        );
+
+        $states = $stateRepository->paginate($query);
+
+        $permissions = [
+            'manage' => 'can_view_state',
+            'create' => 'can_create_state',
+            'update' => 'can_update_state',
+            'delete' => 'can_delete_state',
+        ];
+
+        return view('admin.place.state.index', compact('states', 'permissions'));
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create() :View
+    public function create(CountryRepository $countryRepository) :View
     {
-        //
+        if (!hasPermission('can_create_state')) {
+            $this->unauthorized();
+        }
+
+        $countries=$countryRepository->pluck('name','id')->toArray();
+        return view('admin.place.state.create',compact('countries'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request) : RedirectResponse
+    public function store(StateRequest $request, StateRepository $stateRepository) : RedirectResponse
     {
-        //
+        if (!hasPermission('can_create_state')) {
+            $this->unauthorized();
+        }
+        try {
+            $stateRepository->create($request->validated());
+            return redirect()->route('admin.places.states.index')->with([
+                'message'    => 'State added successfully.',
+                'alert-type' => 'success'
+            ]);
+        } catch (\Exception $e) {
+            return redirect()->back()->with('notification', [
+                [ 'type' => 'error', 'message' => 'State could not be created' ]
+            ]);
+        }
     }
 
     /**
@@ -45,24 +91,66 @@ class StateController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id) : View
+    public function edit(State $state) : View
     {
-        //
+        if (!hasPermission('can_update_state')) {
+            $this->unauthorized();
+        }
+
+        $countries = Country::query()->pluck('name', 'id')->toArray();
+        return view('admin.place.state.edit',compact('state','countries'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id) :RedirectResponse
+    public function update(StateRequest $request, StateRepository $stateRepository, $state) :RedirectResponse
     {
-        //
+
+        if (!hasPermission('can_update_state')) {
+            $this->unauthorized();
+        }
+
+        try {
+            $state= $stateRepository->getModel($state);
+            $stateRepository->update($request->validated(), $state);
+
+            return redirect()->route('admin.places.states.index')->with([
+                'message'    => 'State updated successfully.',
+                'alert-type' => 'success'
+            ]);
+        } catch (\Exception $e) {
+            return redirect()->back()->with([
+                'message'    => 'Something went wrong.',
+                'alert-type' => 'error'
+            ]);
+        }
     }
+
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id) : RedirectResponse
+    public function destroy(StateRepository $stateRepository, $state) : RedirectResponse
     {
-        //
+        if (!hasPermission('can_delete_state')) {
+            $this->unauthorized();
+        }
+
+        try {
+
+            $state = $stateRepository->getModel($state);
+
+            $stateRepository->delete($state->id);
+            return redirect()->route('admin.places.states.index')->with([
+                'message'    => 'State deleted successfully.',
+                'alert-type' => 'success'
+            ]);
+        } catch (\Exception $e) {
+            return redirect()->back()->with([
+                'message'    => 'Something Went Wrong',
+                'alert-type' => 'error'
+            ]);
+        }
     }
 }
--- resources/views/admin/place/state/index.blade.php
+++ resources/views/admin/place/state/index.blade.php
@@ -1,1 +1,70 @@
 
+<x-admin.layout title="States">
+    <x-admin.card>
+        <x-admin.card.card-header title="States" class="d-flex align-content-center">
+            @hasPermission($permissions['create'])
+            <x-admin.page-action>
+                <a href="{{ route('admin.places.states.create') }}" class="m-0 btn btn-xs btn-outline-primary">
+                    <i class="fas fa-plus me-2"></i>
+                    Add State
+                </a>
+            </x-admin.page-action>
+            @endHasPermission
+        </x-admin.card.card-header>
+
+        <x-admin.card.card-body class="px-0 pt-0 pb-2">
+            <x-admin.table>
+                <x-admin.table-header>
+                    <x-admin.table-head value="Name"/>
+                    <x-admin.table-head value="Short Name"/>
+                    <x-admin.table-head value="Country"/>
+                    @if( hasPermission($permissions['update']) || hasPermission($permissions['delete']) )
+                        <x-admin.table-head class="text-right" value="Actions"/>
+                    @endif
+                </x-admin.table-header>
+                <x-admin.table-body>
+                    @forelse($states as $state)
+                        <x-admin.table-row>
+                            <x-admin.table-cell :value="$state->name"/>
+                            <x-admin.table-cell :value="$state->short_name"/>
+                            <x-admin.table-cell>
+                                <x-admin.badge :text="$state->country?->name"/>
+                            </x-admin.table-cell>
+
+
+                            @if( hasPermission($permissions['update']) || hasPermission($permissions['delete']) )
+                                <x-admin.table-cell class="text-right">
+                                    @hasPermission($permissions['update'])
+                                    <a href="{{ route('admin.places.states.edit', $state->id) }}"
+                                       class="btn btn-icon btn-xs btn-outline-primary">
+                                        <i class="fas fa-edit"></i>
+                                    </a>
+                                    @endHasPermission
+                                    @hasPermission($permissions['delete'])
+                                    <x-admin.form
+                                            action="{{ route('admin.places.states.destroy', $state->id) }}"
+                                            method="delete"
+                                            class="d-inline"
+                                    >
+                                        <button onclick="return confirm('Want to delete?')" type="submit"
+                                                class="btn btn-icon btn-xs btn-outline-danger">
+                                            <i class="fas fa-trash"></i>
+                                        </button>
+                                    </x-admin.form>
+                                    @endHasPermission
+                                </x-admin.table-cell>
+                            @endif
+                        </x-admin.table-row>
+                    @empty
+                        <x-admin.table-row>
+                            <x-admin.table-cell colspan="6">No records found.</x-admin.table-cell>
+                        </x-admin.table-row>
+                    @endforelse
+                </x-admin.table-body>
+            </x-admin.table>
+        </x-admin.card.card-body>
+        <x-admin.card.card-footer>
+            {{ $states->links() }}
+        </x-admin.card.card-footer>
+    </x-admin.card>
+</x-admin.layout>
