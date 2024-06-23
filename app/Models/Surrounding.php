1419c7cc973a2b47f31aa08d2f5697ff2ad2a895
Sukanta Purkayastha <spurkayastha83@gmail.com>
2024-06-23 22:42:16 +0600

    Surrounding Seeder added

--- app/Models/Surrounding.php
+++ app/Models/Surrounding.php
@@ -2,7 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 class Surrounding extends Model
 {
-    //
+    /*----------------------------------------
+     * Relationships
+     ----------------------------------------*/
+
+    public function places() : HasMany
+    {
+        return $this->hasMany(SurroundingPlace::class);
+    }
 }
