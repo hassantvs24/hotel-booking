900af964ad6191c7b0b0a2cdef0ef013ac47a29b
Sukanta Purkayastha <spurkayastha83@gmail.com>
2024-08-04 02:06:03 +0600

    properties api ready for ui

---
+++ app/Http/Controllers/API/Portal/PropertyController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\API\Portal;
+
+use App\Http\Controllers\BaseController;
+use App\Models\Property;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class PropertyController extends BaseController
+{
+    public function index() : JsonResponse
+    {
+        $properties = Property::query()
+            ->with(['images', 'facilities'])
+            ->where('status', Property::STATUS_PUBLISHED)
+            ->get();
+
+        $data = [
+            'properties' => $properties
+        ];
+
+        return $this->sendSuccess($data);
+    }
+}
