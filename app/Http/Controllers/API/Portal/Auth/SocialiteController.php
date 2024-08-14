7f81f49e7d92e4cfa42b0555ff122197b934e077
Sukanta Purkayastha <spurkayastha83@gmail.com>
2024-08-14 16:26:32 +0600

    Socialite login with google initially done

---
+++ app/Http/Controllers/API/Portal/Auth/SocialiteController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\API\Portal\Auth;
+
+use App\Http\Controllers\BaseController;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Socialite\Facades\Socialite;
+
+class SocialiteController extends BaseController
+{
+    public function redirectToProvider($provider) : RedirectResponse
+    {
+        return Socialite::driver($provider)->stateless()->redirect();
+    }
+
+    public function handleProviderCallback($provider) : RedirectResponse
+    {
+        $userSocial = Socialite::driver($provider)->stateless()->user();
+
+        $user = User::query()->firstOrCreate(
+            ['email' => $userSocial->getEmail()],
+            [
+                'name'     => $userSocial->getName(),
+                'email'    => $userSocial->getEmail(),
+                'password' => Hash::make('password')
+            ]
+        );
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return redirect()->away(config('app.frontend_url') . '/login/?token=' . $token);
+    }
+}
