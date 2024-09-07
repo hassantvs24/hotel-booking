b8d3f57765e8278174b242f6cba8593637d13023
Sukanta Purkayastha <spurkayastha83@gmail.com>
2024-09-07 14:26:00 +1000

    State CRUD done

--- app/Http/Controllers/API/Admin/Location/StateController.php
+++ app/Http/Controllers/API/Admin/Location/StateController.php
@@ -2,17 +2,40 @@
 
 namespace App\Http\Controllers\API\Admin\Location;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\BaseController;
+use App\Http\Requests\Admin\Place\StateRequest;
+use App\Repositories\Admin\StateRepository;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
-class StateController extends Controller
+class StateController extends BaseController
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request, StateRepository $stateRepository): JsonResponse
     {
-        //
+        $userQuery = array_merge($request->only([
+            'search',
+            'order_by',
+            'order_type',
+            'filters',
+            'page',
+            'per_page'
+        ]),
+            [
+                'order' => 'DESC',
+                'with' => ['country']
+            ]
+        );
+
+        $states = $stateRepository->paginate($userQuery);
+
+        $data = [
+            'states' => $states
+        ];
+
+        return $this->sendSuccess($data);
     }
 
     /**
@@ -26,9 +49,18 @@ class StateController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StateRequest $request, StateRepository $stateRepository): JsonResponse
     {
-        //
+        try {
+            $state = $stateRepository->create($request->validated());
+
+            return $this->sendSuccess($state, 'State created successfully.');
+        } catch (\Exception $e) {
+            return $this->sendError(
+                'State creation failed.',
+                (array)$e->getMessage()
+            );
+        }
     }
 
     /**
@@ -50,16 +82,47 @@ class StateController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(StateRequest $request, StateRepository $stateRepository, $state): JsonResponse
     {
-        //
+        try {
+            $state = $stateRepository->getModel($state);
+            $state = $stateRepository->update($request->validated(), $state);
+
+            return $this->sendSuccess($state, 'State updated successfully.');
+        } catch (\Exception $e) {
+            return $this->sendError(
+                'State update failed.',
+                (array)$e->getMessage()
+            );
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(StateRepository $stateRepository, $state): JsonResponse
     {
-        //
+        try {
+            $state = $stateRepository->getModel($state);
+            $stateRepository->delete($state->id);
+
+            return $this->sendSuccess(null, 'State deleted successfully.');
+        } catch (\Exception $e) {
+            return $this->sendError(
+                'State deletion failed.',
+                (array)$e->getMessage()
+            );
+        }
+    }
+
+    public function all(StateRepository $stateRepository): JsonResponse
+    {
+        $states = $stateRepository->get();
+
+        $data = [
+            'states' => $states
+        ];
+
+        return $this->sendSuccess($data);
     }
 }
--- app/Http/Requests/Admin/Place/StateRequest.php
+++ app/Http/Requests/Admin/Place/StateRequest.php
@@ -22,25 +22,25 @@ class StateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name'      => 'required|string|max:255',
-            'short_name'  => 'required|string|max:30',
+            'name' => 'required|string|max:255',
+            'short_name' => 'required|string|max:30',
             'country_id' => 'required|integer|exists:countries,id',
         ];
     }
 
 
-        /**
+    /**
      * Get the error messages for the defined validation rules.
      *
      * @return array<string, string>
      */
-    public function messages() : array
+    public function messages(): array
     {
         return [
-            'name.required'      => 'State name is required',
-            'name.string'      => 'State name must be a string',
-            'name.max'      => 'State name must not be greater than 255 characters',
-            'short_name.max'      => 'Short name must not be greater than 30 characters',
+            'name.required' => 'State name is required',
+            'name.string' => 'State name must be a string',
+            'name.max' => 'State name must not be greater than 255 characters',
+            'short_name.max' => 'Short name must not be greater than 30 characters',
             'country_id.required' => 'Country is required',
         ];
     }
