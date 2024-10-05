79a926b59405ac3206b486a1aa31871416c75adb
Milton Dey Mishu <114005218+softengmilton@users.noreply.github.com>
2024-10-05 10:01:56 +0100

    Roomrequest  updated

--- app/Http/Controllers/API/Portal/RoomRequestController.php
+++ app/Http/Controllers/API/Portal/RoomRequestController.php
@@ -16,21 +16,41 @@ class RoomRequestController extends BaseController
     public function roomRequest(RoomRequestForm $request): JsonResponse
     {
         $requestData = $request->validated();
-        $data = RoomRequest::updateOrCreate(
-            [
-                'room_id' => $requestData['room_id'],
-                'user_id' => $requestData['user_id'],
-                'property_id' => $requestData['property_id'],
-                
-            ],
-            array_merge($requestData, ['request_expiration_time' => Carbon::now()->addHour(24)])
 
-        );
+        $existingRequest = RoomRequest::where([
+            'room_id' => $requestData['room_id'],
+            'user_id' => $requestData['user_id'],
+            'property_id' => $requestData['property_id'],
+        ])->first();
+
+        if($existingRequest){
+            $data = RoomRequest::updateOrCreate(
+                [
+                    'room_id' => $requestData['room_id'],
+                    'user_id' => $requestData['user_id'],
+                    'property_id' => $requestData['property_id'],
+
+                ],
+                $requestData
+            );
+        }
+        else{
+            $data = RoomRequest::updateOrCreate(
+                [
+                    'room_id' => $requestData['room_id'],
+                    'user_id' => $requestData['user_id'],
+                    'property_id' => $requestData['property_id'],
+
+                ],
+                array_merge($requestData, ['request_expiration_time' => Carbon::now()->addHour(24)])
+            );
+        }
+
         return $this->sendSuccess($data, 'Room request has been created or updated successfully.');
     }
 
 
-    
+
     public function roomRequestNotification(Request $request): JsonResponse
     {
         $roomRequests = RoomRequest::where('user_id', $request->user()->id)     // Fetch the room requests with eager-loaded room and property relations
@@ -43,28 +63,28 @@ class RoomRequestController extends BaseController
                     'request_expiration_time' => $roomRequest->request_expiration_time,
                 ];
             });
-        $uniqueProperties = $roomRequests->groupBy('property_id')->map(function ($requests) {   // Group by property_id and fetch the oldest request_expiration_time for each unique property 
+        $uniqueProperties = $roomRequests->groupBy('property_id')->map(function ($requests) {   // Group by property_id and fetch the oldest request_expiration_time for each unique property
             return $requests->sortBy('request_expiration_time')->first(); // Sort ascending to get the oldest
         });
-    
+
         $data = [
             'properties' => $uniqueProperties->values()->all()        // Prepare response data
         ];
 
         return response()->json($data, 200);
     }
-    
+
 
     public function roomResponselist(Request $request, $propertyId)
     {
-       
+
         $roomRequests = RoomRequest::where('user_id', $request->user()->id)      // Fetch all room requests for the given property and user
             ->where('property_id', $propertyId)
             ->get();
         $totalRequests = $roomRequests->count();        // Count the total number of room requests submitted for the property
 
         $approvedRequests = $roomRequests->where('status', 'Approved')->count();        // Count how many of those requests were approved
-    
+
         $roomRequest = RoomRequestAccepted::whereHas('room_request', function ($query) use ($request, $propertyId) {   // Fetch room responses based on approved room requests
                 $query->where('user_id', $request->user()->id)
                       ->where('status', 'Approved')
@@ -72,22 +92,22 @@ class RoomRequestController extends BaseController
             })
             ->with(['room_request', 'room_request.room', 'room_request.room.primaryImage', 'room_request.room.property.facilities', 'room_request.room.property.place.city'])
             ->get();
-    
+
         $data = [
             'total_requests' => $totalRequests,
             'approved_requests' => $approvedRequests,
             'room_request' => $roomRequest,
         ];
-    
+
         return $this->sendSuccess($data);
     }
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
+
 
 }
