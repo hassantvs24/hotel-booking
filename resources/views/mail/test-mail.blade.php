ece58f223160c51a785f9a0e7a716ceaefba80f2
Sukanta Purkayastha <spurkayastha83@gmail.com>
2024-11-18 04:09:08 +1100

    test mail created with mail trap

---
+++ resources/views/mail/test-mail.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Test Mail</h1>
+    <p>This is a test mail from Laravel. Please ignore it.</p>
+    <p>Thank you.</p>
+</body>
+</html>
